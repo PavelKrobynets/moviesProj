2a60dd5c262ce44ea6e7a385f067689e0f6fdcdd
Pavel <krobynetspavel@gmail.com>
2024-10-28 12:41:07 +0200

    slider done

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,7 +1,11 @@
 import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
 import useRequest from "../../hooks/useRequest";
 import { useEffect, useState } from "react";
 import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+import "./slider.scss";
 
 interface Movie {
   backdrop_path: string;
@@ -11,28 +15,43 @@ interface Movie {
 
 export default function Slider() {
   const [movies, setMovies] = useState<Movie[] | null>(null);
+  const [slidesPerView, setSlidesPerView] = useState<number>(2);
   const request = useRequest();
 
   useEffect(() => {
     request.fetchTrendingMovies().then((res) => setMovies(res.results));
+
+    const handleResize = () => {
+      if (window.innerWidth > 1440) {
+        setSlidesPerView(3);
+      }
+    };
+    window.addEventListener("resize", handleResize);
+    return () => {
+      window.removeEventListener("resize", handleResize);
+    };
   }, []);
 
   return (
     <Swiper
-      className="swiper"
-      spaceBetween={50}
-      slidesPerView={1}
-      onSlideChange={() => console.log("slide change")}
-      onSwiper={(swiper) => console.log(swiper)}
+      className="slider"
+      modules={[Navigation, Pagination, Autoplay]}
+      slidesPerView={slidesPerView}
+      spaceBetween={0}
+      autoplay={true}
+      navigation={true}
+      pagination={{
+        clickable: true,
+      }}
     >
       {movies?.map((movie) => (
         <SwiperSlide key={movie.id} className="slider__slide">
           <img
-            className="slider_img"
+            className="slider__slide-img"
             src={`https://image.tmdb.org/t/p/w500/${movie.backdrop_path}`}
             alt={`Movie backdrop for ${movie.id}`}
           />
-          <div className="slide__title">{movie.title}</div>
+          <div className="slider__slide-title">{movie.title}</div>
         </SwiperSlide>
       ))}
     </Swiper>
